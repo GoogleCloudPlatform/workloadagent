40bbc885b092881c285ac97923e74173ec87150c
Google Cloud Workload Agent Dev <workloadagent-github-dev@google.com>
2025-01-19 22:12:06 -0800

    Added logic to call WorkloadCollector from Daemon file and passing workload manager client and agent cloud properties which will be used to call writeInsight api for sending data to Data Warehouse.
    PiperOrigin-RevId: 717396954

--- internal/daemon/daemon.go
+++ internal/daemon/daemon.go
@@ -35,6 +35,8 @@ import (
 	"github.com/GoogleCloudPlatform/workloadagent/internal/servicecommunication/discovery"
 	"github.com/GoogleCloudPlatform/workloadagent/internal/servicecommunication"
 	"github.com/GoogleCloudPlatform/workloadagent/internal/usagemetrics"
+	"github.com/GoogleCloudPlatform/workloadagent/internal/workloadmanager"
+	"github.com/GoogleCloudPlatform/workloadagentplatform/integration/common/shared/gce/wlm"
 	"github.com/GoogleCloudPlatform/workloadagentplatform/integration/common/shared/log"
 	"github.com/GoogleCloudPlatform/workloadagentplatform/integration/common/shared/recovery"
 
@@ -189,10 +191,37 @@ func (d *Daemon) startdaemonHandler(ctx context.Context, cancel context.CancelFu
 		recoverableStart.StartRoutine(ctx)
 	}
 
+	recoverableStart = &recovery.RecoverableRoutine{
+		Routine:             startMetricsCollection,
+		RoutineArg:          d.config,
+		ErrorCode:           0,
+		ExpectedMinDuration: 0,
+		UsageLogger:         *usagemetrics.UsageLogger,
+	}
+	recoverableStart.StartRoutine(ctx)
+
 	// Log a RUNNING usage metric once a day.
 	go usagemetrics.LogRunningDaily()
 	d.waitForShutdown(shutdownch, cancel)
 	return nil
+}
+
+/*
+ * startMetricsCollection handles the logic for overriding Workload Manager (WLM) metrics.
+ * Future enhancements will include the collection of actual WLM metrics.
+ *
+ * Initializes a new Workload Manager (WLM) client and initiates the metric collection process.
+ */
+func startMetricsCollection(ctx context.Context, a any) {
+	config := a.(*cpb.Configuration)
+	wlmService, err := wlm.NewWLMClient(ctx, config.GetDataWarehouseEndpoint())
+	if err != nil {
+		log.Logger.Errorw("Error creating WLM Client", "error", err)
+		usagemetrics.Error(usagemetrics.WorkloadManagerConnectionError)
+		return
+	}
+	wmCtx := log.SetCtx(ctx, "context", "WorkloadManagerMetrics")
+	workloadmanager.CollectAndSendMetricsToDataWarehouse(wmCtx, wlmService, config.GetCloudProperties())
 }
 
 // configureUsageMetricsForDaemon sets up UsageMetrics for Daemon.
--- internal/workloadmanager/workloadcollector.go
+++ internal/workloadmanager/workloadcollector.go
@@ -19,16 +19,45 @@ package workloadmanager
 
 import (
 	"bufio"
+	"bytes"
 	"context"
 	"io"
+	"os"
 
+	cpb "github.com/GoogleCloudPlatform/workloadagent/protos/configuration"
 	"github.com/GoogleCloudPlatform/workloadagentplatform/integration/common/shared/log"
+	dwpb "github.com/GoogleCloudPlatform/workloadagentplatform/integration/common/shared/protos/datawarehouse"
 )
 
 // ConfigFileReader is a function that reads a config file.
 type ConfigFileReader func(string) (io.ReadCloser, error)
 
+// WorkloadMetrics is a struct that collect data from override configuration file for testing purposes.
+// Future enhancements will include the collection of actual WLM metrics.
+type WorkloadMetrics struct {
+}
+
+type wlmWriter interface {
+	WriteInsight(project, location string, writeInsightRequest *dwpb.WriteInsightRequest) error
+}
+
+// sendMetricsParams defines the set of parameters required to call sendMetrics
+type sendMetricsParams struct {
+	wm         WorkloadMetrics
+	cp         *cpb.CloudProperties
+	wlmService wlmWriter
+}
+
 const metricOverridePath = "/etc/google-cloud-workload-agent/wlmmetricoverride.yaml"
+
+// CollectAndSendMetricsToDataWarehouse collects workload metrics and sends them to Data Warehouse.
+func CollectAndSendMetricsToDataWarehouse(ctx context.Context, wlmService wlmWriter, cp *cpb.CloudProperties) {
+	if !readAndLogMetricOverrideYAML(ctx, readFileWrapper) {
+		return
+	}
+	// TODO - Parse the metric override file and create the workload metrics.
+	// TODO - Send the metrics to Data Warehouse.
+}
 
 func readAndLogMetricOverrideYAML(ctx context.Context, reader ConfigFileReader) bool {
 	file, err := reader(metricOverridePath)
@@ -50,4 +79,12 @@ func readAndLogMetricOverrideYAML(ctx context.Context, reader ConfigFileReader) 
 	}
 
 	return true
+}
+
+func readFileWrapper(path string) (io.ReadCloser, error) {
+	content, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	return io.NopCloser(bytes.NewReader(content)), nil
 }
