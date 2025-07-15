a597b06d1ca55952db65a4cadfe5afaaa7058124
Google Cloud Agent for Compute Workloads Dev <workloadagent-github-dev@google.com>
2025-07-15 08:56:04 -0700

    Point database center UAP channel to prod endpoint.
    PiperOrigin-RevId: 783351585

--- internal/databasecenter/databasecenter.go
+++ internal/databasecenter/databasecenter.go
@@ -34,8 +34,8 @@ import (
 
 const (
 	endpoint = "" // endpoint override for database center, don't set if not needed
-	// TODO: Update the channel to prod channel once the integration is tested.
-	channel = "databasecenter.googleapis.com/dbcenter-autopush"
+	// DB Center UAP channel name.
+	channel = "databasecenter.googleapis.com/dbcenter-prod"
 	// MajorVersionKey is the key for the major version in metrics.
 	MajorVersionKey = "major_version"
 	// MinorVersionKey is the key for the minor version in metrics.
