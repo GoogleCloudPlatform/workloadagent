2378bfd5b21537320ac6ad6241958f138047d301
Google Cloud Agent for Compute Workloads Dev <workloadagent-github-dev@google.com>
2025-07-21 13:44:00 -0700

    Add status command to check Installed and Available Version.
    PiperOrigin-RevId: 785560101

--- internal/onetime/status/status.go
+++ internal/onetime/status/status.go
@@ -19,9 +19,12 @@ package status
 
 import (
 	"context"
+	"fmt"
 	"runtime"
 
+	"cloud.google.com/go/artifactregistry/apiv1"
 	"github.com/spf13/cobra"
+	"github.com/GoogleCloudPlatform/workloadagent/internal/daemon/configuration"
 	"github.com/GoogleCloudPlatform/workloadagentplatform/sharedlibraries/commandlineexecutor"
 	"github.com/GoogleCloudPlatform/workloadagentplatform/sharedlibraries/log"
 	"github.com/GoogleCloudPlatform/workloadagentplatform/sharedlibraries/statushelper"
@@ -29,11 +32,18 @@ import (
 	spb "github.com/GoogleCloudPlatform/workloadagentplatform/sharedprotos/status"
 )
 
-const agentPackageName = "google-cloud-workload-agent"
+const (
+	agentPackageName        = "google-cloud-workload-agent"
+	projectName             = "workload-agent-products"
+	repositoryLocation      = "us"
+	repositoryName          = "google-cloud-workload-agent-x86-64"
+	fetchLatestVersionError = "Error: could not fetch latest version"
+)
 
 // Status stores the status command parameters.
 type Status struct {
-	exec commandlineexecutor.Execute
+	exec     commandlineexecutor.Execute
+	arClient statushelper.ARClientInterface
 }
 
 // NewCommand creates a new status command.
@@ -45,6 +55,9 @@ func NewCommand() *cobra.Command {
 		Long:  "Print the status of the agent",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			ctx := cmd.Context()
+			if err := s.Init(ctx); err != nil {
+				return err
+			}
 			s.exec = commandlineexecutor.ExecuteCommand
 			statushelper.PrintStatus(ctx, s.agentStatus(ctx), false)
 			return nil
@@ -53,10 +66,32 @@ func NewCommand() *cobra.Command {
 	return cmd
 }
 
+// Init creates clients for artifact registry.
+func (s *Status) Init(ctx context.Context) error {
+	arClient, err := artifactregistry.NewClient(ctx)
+	if err != nil {
+		log.CtxLogger(ctx).Errorw("Could not create artifact registry client", "error", err)
+		return err
+	}
+	s.arClient = &statushelper.ArtifactRegistryClient{Client: arClient}
+	return nil
+}
+
 // agentStatus returns the agent version, enabled/running, config path, and the
 // configuration as parsed by the agent.
 func (s *Status) agentStatus(ctx context.Context) *spb.AgentStatus {
-	agentStatus := &spb.AgentStatus{}
+	agentStatus := &spb.AgentStatus{
+		AgentName:        agentPackageName,
+		InstalledVersion: fmt.Sprintf("%s-%s", configuration.AgentVersion, configuration.AgentBuildChange),
+	}
+
+	var err error
+	agentStatus.AvailableVersion, err = statushelper.LatestVersionArtifactRegistry(ctx, s.arClient, projectName, repositoryLocation, repositoryName, agentPackageName)
+	if err != nil {
+		log.CtxLogger(ctx).Errorw("Could not fetch latest version", "error", err)
+		agentStatus.AvailableVersion = fetchLatestVersionError
+	}
+
 	agentStatus.SystemdServiceEnabled = spb.State_FAILURE_STATE
 	agentStatus.SystemdServiceRunning = spb.State_FAILURE_STATE
 	enabled, running, err := statushelper.CheckAgentEnabledAndRunning(ctx, agentPackageName, runtime.GOOS, s.exec)
