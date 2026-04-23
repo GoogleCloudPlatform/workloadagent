52e8badabde0a27c54f61dddd35d263fa1608cb6
Google Cloud Agent for Compute Workloads Dev <workloadagent-github-dev@google.com>
2026-04-22 23:32:02 -0700

    Collect additional resource metadata for Secret Manager WLM rules in OpenShift clusters
    PiperOrigin-RevId: 904253927

--- internal/openshiftmetrics/openshiftmetrics.go
+++ internal/openshiftmetrics/openshiftmetrics.go
@@ -20,8 +20,12 @@ package openshiftmetrics
 import (
 	"context"
 	"fmt"
+	"reflect"
 
 	apiextensions "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"google.golang.org/protobuf/encoding/protojson"
@@ -51,6 +55,8 @@ type OpenShiftMetrics struct {
 	OpenShiftClient *openshift.Client
 	// APIExtensionsClient is the client for the API extensions group.
 	APIExtensionsClient *apiextensions.Clientset
+	// DynamicClient is the client for dynamic resources.
+	DynamicClient dynamic.Interface
 }
 
 // MetricVersioning contains the versioning information for the metric agent and payload.
@@ -93,6 +99,12 @@ func (o *OpenShiftMetrics) Init(ctx context.Context) error {
 		return err
 	}
 	o.APIExtensionsClient = apiExtensionsClient
+
+	dynamicClient, err := dynamic.NewForConfig(config)
+	if err != nil {
+		return err
+	}
+	o.DynamicClient = dynamicClient
 
 	return nil
 }
@@ -162,6 +174,26 @@ func (o *OpenShiftMetrics) CollectMetrics(ctx context.Context, versionData Metri
 	if err := o.collectNodes(ctx, payload); err != nil {
 		logger.Warnw("Failed to collect nodes data", "error", err)
 		usagemetrics.Error(usagemetrics.OpenShiftMetricCollectionFailure)
+	}
+
+	if err := o.collectDaemonSets(ctx, namespaces, payload); err != nil {
+		logger.Warnw("Failed to collect daemon sets data", "error", err)
+	}
+
+	if err := o.collectSecretProviderClasses(ctx, namespaces, payload); err != nil {
+		logger.Warnw("Failed to collect secret provider classes data", "error", err)
+	}
+
+	if err := o.collectAPIServers(ctx, payload); err != nil {
+		logger.Warnw("Failed to collect api servers data", "error", err)
+	}
+
+	if err := o.collectSecretStores(ctx, namespaces, payload); err != nil {
+		logger.Warnw("Failed to collect secret stores data", "error", err)
+	}
+
+	if err := o.collectExternalSecrets(ctx, namespaces, payload); err != nil {
+		logger.Warnw("Failed to collect external secrets data", "error", err)
 	}
 
 	logger.Debugw("Metric payload after collection", "payload", payload)
@@ -382,8 +414,8 @@ func (o *OpenShiftMetrics) collectDeploymentData(ctx context.Context, namespaces
 					}
 				}
 				volumes = append(volumes, &ompb.Volume{
-					Name:   volume.Name,
-					Secret: secret,
+					Name:      volume.Name,
+					Secret:    secret,
 					Projected: projected,
 				})
 			}
@@ -759,6 +791,304 @@ func (o *OpenShiftMetrics) collectNodes(ctx context.Context, payload *ompb.Opens
 		ApiVersion:     nodes.APIVersion,
 		Metadata:       &ompb.ResourceListContainer_Metadata{ResourceVersion: nodes.ResourceVersion},
 		ContainerItems: &ompb.ResourceListContainer_Nodes{Nodes: &ompb.NodeList{Items: nodeList}},
+	}
+
+	return nil
+}
+
+// collectDaemonSets collects specific daemon sets from the cluster.
+func (o *OpenShiftMetrics) collectDaemonSets(ctx context.Context, namespaces []string, payload *ompb.OpenshiftMetricsPayload) error {
+	var daemonSetList []*ompb.DaemonSet
+	var kind, apiVersion, resourceVersion string
+
+	for _, ns := range namespaces {
+		daemonSets, err := o.K8sClient.AppsV1().DaemonSets(ns).List(ctx, metav1.ListOptions{})
+		if err != nil {
+			return err
+		}
+
+		kind = daemonSets.Kind
+		apiVersion = daemonSets.APIVersion
+		resourceVersion = daemonSets.ResourceVersion
+
+		for _, ds := range daemonSets.Items {
+			if ds.Name != "csi-secrets-store-provider-gcp" {
+				continue
+			}
+
+			var containers []*ompb.Container
+			for _, container := range ds.Spec.Template.Spec.Containers {
+				var env []*ompb.Env
+				for _, e := range container.Env {
+					var valueFrom *ompb.Env_ValueFrom
+					if e.ValueFrom != nil && e.ValueFrom.SecretKeyRef != nil {
+						valueFrom = &ompb.Env_ValueFrom{
+							SecretKeyRef: &ompb.Env_ValueFrom_SecretKeyRef{
+								Name: e.ValueFrom.SecretKeyRef.Name,
+							},
+						}
+					}
+					env = append(env, &ompb.Env{
+						ValueFrom: valueFrom,
+						Name:      e.Name,
+						Value:     e.Value,
+					})
+				}
+				containers = append(containers, &ompb.Container{
+					Env: env,
+				})
+			}
+
+			daemonSetList = append(daemonSetList, &ompb.DaemonSet{
+				Metadata: &ompb.ResourceMetadata{
+					Name:              ds.Name,
+					Uid:               string(ds.UID),
+					ResourceVersion:   ds.ResourceVersion,
+					Generation:        ds.Generation,
+					CreationTimestamp: tspb.New(ds.CreationTimestamp.Time),
+					Labels:            ds.Labels,
+					Annotations:       ds.Annotations,
+					Namespace:         ns,
+				},
+				Spec: &ompb.DaemonSet_Spec{
+					PodTemplate: &ompb.PodTemplate{
+						Metadata: &ompb.ResourceMetadata{
+							Name:              ds.Spec.Template.Name,
+							CreationTimestamp: tspb.New(ds.Spec.Template.CreationTimestamp.Time),
+							Labels:            ds.Spec.Template.Labels,
+							Annotations:       ds.Spec.Template.Annotations,
+						},
+						Spec: &ompb.PodSpec{
+							Containers: containers,
+						},
+					},
+				},
+			})
+			break
+		}
+	}
+
+	payload.DaemonSets = &ompb.ResourceListContainer{
+		Kind:           kind,
+		ApiVersion:     apiVersion,
+		Metadata:       &ompb.ResourceListContainer_Metadata{ResourceVersion: resourceVersion},
+		ContainerItems: &ompb.ResourceListContainer_DaemonSets{DaemonSets: &ompb.DaemonSetList{Items: daemonSetList}},
+	}
+
+	return nil
+}
+
+// collectSecretProviderClasses collects SecretProviderClass resources from the cluster.
+func (o *OpenShiftMetrics) collectSecretProviderClasses(ctx context.Context, namespaces []string, payload *ompb.OpenshiftMetricsPayload) error {
+	var spcList []*ompb.SecretProviderClass
+	var kind, apiVersion, resourceVersion string
+
+	gvr := schema.GroupVersionResource{Group: "secrets-store.csi.x-k8s.io", Version: "v1", Resource: "secretproviderclasses"}
+
+	for _, ns := range namespaces {
+		list, err := o.DynamicClient.Resource(gvr).Namespace(ns).List(ctx, metav1.ListOptions{})
+		if err != nil {
+			continue
+		}
+
+		kind = list.GetKind()
+		apiVersion = list.GetAPIVersion()
+		resourceVersion = list.GetResourceVersion()
+
+		for _, item := range list.Items {
+			var secretObjects []*ompb.SecretProviderClass_SecretObject
+
+			so, found, err := unstructured.NestedFieldNoCopy(item.Object, "spec", "secretObjects")
+			if err == nil && found {
+				// Use reflection to count elements in the slice, ignoring specific element types
+				// (e.g., []interface{} vs []map[string]interface{}). Assuming any one type could break if
+				// the underlying type changes.
+				val := reflect.ValueOf(so)
+				if val.Kind() == reflect.Slice {
+					for i := 0; i < val.Len(); i++ {
+						secretObjects = append(secretObjects, &ompb.SecretProviderClass_SecretObject{})
+					}
+				} else {
+					log.CtxLogger(ctx).Debugw("Unknown type for secretObjects", "type", fmt.Sprintf("%T", so))
+				}
+			}
+
+			provider, _, _ := unstructured.NestedString(item.Object, "spec", "provider")
+
+			spcList = append(spcList, &ompb.SecretProviderClass{
+				Metadata: &ompb.ResourceMetadata{
+					Name:              item.GetName(),
+					Uid:               string(item.GetUID()),
+					ResourceVersion:   item.GetResourceVersion(),
+					Generation:        item.GetGeneration(),
+					CreationTimestamp: tspb.New(item.GetCreationTimestamp().Time),
+					Labels:            item.GetLabels(),
+					Annotations:       item.GetAnnotations(),
+					Namespace:         ns,
+				},
+				Spec: &ompb.SecretProviderClass_Spec{
+					SecretObjects: secretObjects,
+					Provider:      provider,
+				},
+			})
+		}
+	}
+
+	payload.SecretProviderClasses = &ompb.ResourceListContainer{
+		Kind:           kind,
+		ApiVersion:     apiVersion,
+		Metadata:       &ompb.ResourceListContainer_Metadata{ResourceVersion: resourceVersion},
+		ContainerItems: &ompb.ResourceListContainer_SecretProviderClasses{SecretProviderClasses: &ompb.SecretProviderClassList{Items: spcList}},
+	}
+
+	return nil
+}
+
+// collectAPIServers collects APIServer resources from the cluster.
+func (o *OpenShiftMetrics) collectAPIServers(ctx context.Context, payload *ompb.OpenshiftMetricsPayload) error {
+	var apiServerList []*ompb.APIServer
+
+	resp, err := o.OpenShiftClient.GetAPIServer()
+	if err != nil {
+		return err
+	}
+
+	apiServerList = append(apiServerList, &ompb.APIServer{
+		Metadata: &ompb.ResourceMetadata{
+			Name: resp.Metadata.Name,
+			Uid:  resp.Metadata.UID,
+		},
+		Spec: &ompb.APIServer_Spec{
+			Encryption: &ompb.APIServer_Encryption{
+				Type: resp.Spec.Encryption.Type,
+			},
+		},
+	})
+
+	payload.ApiServers = &ompb.ResourceListContainer{
+		Kind:           resp.Kind,
+		ApiVersion:     resp.APIVersion,
+		Metadata:       &ompb.ResourceListContainer_Metadata{ResourceVersion: resp.Metadata.ResourceVersion},
+		ContainerItems: &ompb.ResourceListContainer_ApiServers{ApiServers: &ompb.APIServerList{Items: apiServerList}},
+	}
+
+	return nil
+}
+
+// collectSecretStores collects SecretStore resources from the cluster.
+func (o *OpenShiftMetrics) collectSecretStores(ctx context.Context, namespaces []string, payload *ompb.OpenshiftMetricsPayload) error {
+	var ssList []*ompb.SecretStore
+	var kind, apiVersion, resourceVersion string
+
+	gvr := schema.GroupVersionResource{Group: "external-secrets.io", Version: "v1", Resource: "secretstores"}
+
+	for _, ns := range namespaces {
+		list, err := o.DynamicClient.Resource(gvr).Namespace(ns).List(ctx, metav1.ListOptions{})
+		if err != nil {
+			continue
+		}
+
+		kind = list.GetKind()
+		apiVersion = list.GetAPIVersion()
+		resourceVersion = list.GetResourceVersion()
+
+		for _, item := range list.Items {
+			var gcpsm *ompb.SecretStore_GcpSm
+
+			spec, found, err := unstructured.NestedMap(item.Object, "spec")
+			if err == nil && found {
+				if provider, ok := spec["provider"].(map[string]any); ok {
+					if _, ok := provider["gcpsm"].(map[string]any); ok {
+						gcpsm = &ompb.SecretStore_GcpSm{}
+					}
+				}
+			}
+
+			ssList = append(ssList, &ompb.SecretStore{
+				Metadata: &ompb.ResourceMetadata{
+					Name:              item.GetName(),
+					Uid:               string(item.GetUID()),
+					ResourceVersion:   item.GetResourceVersion(),
+					Generation:        item.GetGeneration(),
+					CreationTimestamp: tspb.New(item.GetCreationTimestamp().Time),
+					Labels:            item.GetLabels(),
+					Annotations:       item.GetAnnotations(),
+					Namespace:         ns,
+				},
+				Spec: &ompb.SecretStore_Spec{
+					Provider: &ompb.SecretStore_Provider{
+						Gcpsm: gcpsm,
+					},
+				},
+			})
+		}
+	}
+
+	payload.SecretStores = &ompb.ResourceListContainer{
+		Kind:           kind,
+		ApiVersion:     apiVersion,
+		Metadata:       &ompb.ResourceListContainer_Metadata{ResourceVersion: resourceVersion},
+		ContainerItems: &ompb.ResourceListContainer_SecretStores{SecretStores: &ompb.SecretStoreList{Items: ssList}},
+	}
+
+	return nil
+}
+
+// collectExternalSecrets collects ExternalSecret resources from the cluster.
+func (o *OpenShiftMetrics) collectExternalSecrets(ctx context.Context, namespaces []string, payload *ompb.OpenshiftMetricsPayload) error {
+	var esList []*ompb.ExternalSecret
+	var kind, apiVersion, resourceVersion string
+
+	gvr := schema.GroupVersionResource{Group: "external-secrets.io", Version: "v1", Resource: "externalsecrets"}
+
+	for _, ns := range namespaces {
+		list, err := o.DynamicClient.Resource(gvr).Namespace(ns).List(ctx, metav1.ListOptions{})
+		if err != nil {
+			continue
+		}
+
+		kind = list.GetKind()
+		apiVersion = list.GetAPIVersion()
+		resourceVersion = list.GetResourceVersion()
+
+		for _, item := range list.Items {
+			var secretStoreRef *ompb.ExternalSecret_SecretStoreRef
+
+			spec, found, err := unstructured.NestedMap(item.Object, "spec")
+			if err == nil && found {
+				if ref, ok := spec["secretStoreRef"].(map[string]any); ok {
+					name, _ := ref["name"].(string)
+					kind, _ := ref["kind"].(string)
+					secretStoreRef = &ompb.ExternalSecret_SecretStoreRef{
+						Name: name,
+						Kind: kind,
+					}
+				}
+			}
+
+			esList = append(esList, &ompb.ExternalSecret{
+				Metadata: &ompb.ResourceMetadata{
+					Name:              item.GetName(),
+					Uid:               string(item.GetUID()),
+					ResourceVersion:   item.GetResourceVersion(),
+					Generation:        item.GetGeneration(),
+					CreationTimestamp: tspb.New(item.GetCreationTimestamp().Time),
+					Labels:            item.GetLabels(),
+					Annotations:       item.GetAnnotations(),
+					Namespace:         ns,
+				},
+				Spec: &ompb.ExternalSecret_Spec{
+					SecretStoreRef: secretStoreRef,
+				},
+			})
+		}
+	}
+
+	payload.ExternalSecrets = &ompb.ResourceListContainer{
+		Kind:           kind,
+		ApiVersion:     apiVersion,
+		Metadata:       &ompb.ResourceListContainer_Metadata{ResourceVersion: resourceVersion},
+		ContainerItems: &ompb.ResourceListContainer_ExternalSecrets{ExternalSecrets: &ompb.ExternalSecretList{Items: esList}},
 	}
 
 	return nil
