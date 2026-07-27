622d003612d56df40d16f9f345aeba422cc15c5e
Google Cloud Agent for Compute Workloads Dev <workloadagent-github-dev@google.com>
2026-07-27 12:20:50 -0700

    MySQL metrics - send Database Center metrics to Data Warehouse.
    PiperOrigin-RevId: 954764762

--- internal/mysqlmetrics/mysqlmetrics.go
+++ internal/mysqlmetrics/mysqlmetrics.go
@@ -46,6 +46,7 @@ const (
 	replicationZonesKey           = "replication_zones"
 	notProtectedByAutoFailoverKey = "not_protected_by_auto_failover"
 	noAutomatedBackupPolicyKey    = "no_automated_backup_policy"
+	lastBackupOldKey              = "last_backup_old"
 	sourceRole                    = "source"
 	replicaRole                   = "replica"
 	replicationZonesQuery         = "SELECT HOST FROM information_schema.PROCESSLIST AS p WHERE p.COMMAND = 'Binlog Dump'"
@@ -1144,6 +1145,10 @@ func (m *MySQLMetrics) CollectWlmMetricsOnce(ctx context.Context, dwActivated bo
 	if err != nil {
 		log.CtxLogger(ctx).Warnf("Failed to get no automated backup policy: %v", err)
 	}
+	lastBackupOld, err := m.lastBackupOld(ctx)
+	if err != nil {
+		log.CtxLogger(ctx).Warnf("Failed to get last backup old: %v", err)
+	}
 	log.CtxLogger(ctx).Debugw("Finished collecting MySQL metrics once. Next step is to send to WLM (DW).",
 		bufferPoolKey, bufferPoolSize,
 		totalRAMKey, totalRAM,
@@ -1152,6 +1157,7 @@ func (m *MySQLMetrics) CollectWlmMetricsOnce(ctx context.Context, dwActivated bo
 		replicationZonesKey, strings.Join(replicationZones, ","),
 		notProtectedByAutoFailoverKey, notProtectedByAutoFailover,
 		noAutomatedBackupPolicyKey, noAutomatedBackupPolicy,
+		lastBackupOldKey, lastBackupOld,
 	)
 	metrics := workloadmanager.WorkloadMetrics{
 		WorkloadType: workloadmanager.MYSQL,
@@ -1163,6 +1169,7 @@ func (m *MySQLMetrics) CollectWlmMetricsOnce(ctx context.Context, dwActivated bo
 			replicationZonesKey:           strings.Join(replicationZones, ","),
 			notProtectedByAutoFailoverKey: strconv.FormatBool(notProtectedByAutoFailover),
 			noAutomatedBackupPolicyKey:    strconv.FormatBool(noAutomatedBackupPolicy),
+			lastBackupOldKey:              strconv.FormatBool(lastBackupOld),
 		},
 	}
 	res, err := workloadmanager.SendDataInsight(ctx, workloadmanager.SendDataInsightParams{
