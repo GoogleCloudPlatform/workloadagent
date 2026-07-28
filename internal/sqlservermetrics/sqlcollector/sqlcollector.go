9bf866ff7f40fa400f9f48a7d915cac366f683c4
Google Cloud Agent for Compute Workloads Dev <workloadagent-github-dev@google.com>
2026-07-28 04:21:15 -0700

    [WLM for SQL] Add rule for checking encrypted connections enforced
    PiperOrigin-RevId: 955179063

--- internal/sqlservermetrics/sqlcollector/sqlcollector.go
+++ internal/sqlservermetrics/sqlcollector/sqlcollector.go
@@ -80,6 +80,40 @@ DBStatuses AS (
 	  AND d.state_desc = 'ONLINE'
 )`
 
+// auditingEnabledCTE evaluates whether database auditing is enabled on the SQL Server instance.
+const auditingEnabledCTE = `AuditingStatus AS (
+	SELECT
+		CASE
+			WHEN COUNT(*) > 0 THEN 1
+			ELSE 0
+		END AS auditing_enabled
+	FROM sys.server_audits sa
+	WHERE sa.is_state_enabled = 1
+	  AND (
+		EXISTS (SELECT 1
+			FROM sys.server_audit_specifications sas
+			WHERE sas.audit_guid = sa.audit_guid
+			  AND sas.is_state_enabled = 1)
+		OR
+		EXISTS (SELECT 1
+			FROM sys.database_audit_specifications das
+			WHERE das.audit_guid = sa.audit_guid
+			  AND das.is_state_enabled = 1)
+	  )
+)`
+
+// allowsUnencryptedConnectionsCTE evaluates whether unencrypted connections exist on the SQL Server instance.
+const allowsUnencryptedConnectionsCTE = `UnencryptedConnectionsStatus AS (
+	SELECT
+		CASE
+			WHEN COUNT(*) > 0 THEN 1
+			ELSE 0
+		END AS allows_unencrypted_connections
+	FROM sys.dm_exec_connections
+	WHERE encrypt_option = 'FALSE'
+	  AND net_transport <> 'Shared memory'
+)`
+
 // SQLMetrics defines the rules the agent will collect from sql server.
 var SQLMetrics = []SQLMetricsStruct{
 	{
@@ -254,7 +288,7 @@ var SQLMetrics = []SQLMetricsStruct{
 	},
 	{
 		Name: "INSTANCE_METRICS",
-		Query: fmt.Sprintf(`WITH %s
+		Query: fmt.Sprintf(`WITH %s, %s, %s
 						SELECT
 							SERVERPROPERTY('productversion') AS productversion,
 							SERVERPROPERTY ('productlevel') AS productlevel,
@@ -266,8 +300,10 @@ var SQLMetrics = []SQLMetricsStruct{
 							socket_count AS socketCount,
 							cores_per_socket AS coresPerSocket,
 							numa_node_count AS numaNodeCount,
-							ISNULL((SELECT CASE WHEN SUM(CASE WHEN FailoverProtectionStatus = 'UNPROTECTED' THEN 1 ELSE 0 END) > 0 THEN 1 ELSE 0 END FROM DBStatuses), 0) AS not_protected_by_auto_failover
-						FROM sys.dm_os_sys_info`, notProtectedByAutoFailoverCTE),
+							ISNULL((SELECT CASE WHEN SUM(CASE WHEN FailoverProtectionStatus = 'UNPROTECTED' THEN 1 ELSE 0 END) > 0 THEN 1 ELSE 0 END FROM DBStatuses), 0) AS not_protected_by_auto_failover,
+							ISNULL((SELECT CASE WHEN auditing_enabled = 1 THEN 0 ELSE 1 END FROM AuditingStatus), 1) AS auditing_not_enabled,
+							ISNULL((SELECT allows_unencrypted_connections FROM UnencryptedConnectionsStatus), 1) AS allows_unencrypted_connections
+						FROM sys.dm_os_sys_info`, notProtectedByAutoFailoverCTE, auditingEnabledCTE, allowsUnencryptedConnectionsCTE),
 		Fields: func(fields [][]any) []map[string]string {
 			res := []map[string]string{}
 			for _, f := range fields {
@@ -283,7 +319,9 @@ var SQLMetrics = []SQLMetricsStruct{
 					"cores_per_socket":               handleNilInt(f[8]),
 					"numa_node_count":                handleNilInt(f[9]),
 					"not_protected_by_auto_failover": handleNilInt(f[10]),
-					"os":                             handleNilString(f[11]),
+					"auditing_not_enabled":           handleNilInt(f[11]),
+					"allows_unencrypted_connections": handleNilInt(f[12]),
+					"os":                             handleNilString(f[13]),
 				})
 			}
 			return res
